c55008a4022af484736d44997080eafde227dc86
mIksrigz <m.iksrigz@gmail.com>
2023-10-12 08:07:58 +0200

    animation and movement added

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+let score = 0;
+ctx.font = '50px Impact';
 
 let timeToNextRaven = 0;
 let ravenInterval = 500;
@@ -12,22 +14,54 @@ let lastTime = 0;
 let ravens = [];
 class Raven {
     constructor() {
-        this.width = 100;
-        this.height = 50;
+        this.spriteWidth = 271;
+        this.spriteHeight = 194;
+        this.sizeModifier = Math.random() * 0.6 + 0.4;
+        this.width = this.spriteWidth * this.sizeModifier;
+        this.height = this.spriteHeight * this.sizeModifier;
         this.x = canvas.width;
         this.y = Math.random() * (canvas.height - this.height);
         this.directionX = Math.random() * 5 + 3;
         this.directionY = Math.random() * 5 - 2.5;
         this.markedForDeletion = false;
+        this.image = new Image();
+        this.image.src = 'raven.png';
+        this.frame = 0;
+        this.maxFrame = 4;
+        this.timeSinceFlap = 0;
+        this.flapInterval = Math.random() * 50 + 50;
     }
-    update() {
+    update(deltatime) {
+        if(this.y < 0 || this.y > canvas.height -this.height){
+            this.directionY = this.directionY * -1;
+        } 
         this.x -= this.directionX;
+        this.y += this.directionY;
         if (this.x < 0 - this.width) this.markedForDeletion = true;
+        this.timeSinceFlap += deltatime;
+        if (this.timeSinceFlap > this.flapInterval) {
+            if (this.frame > this.maxFrame) this.frame = 0;
+            else this.frame++;
+            this.timeSinceFlap = 0;
+        }
     }
     draw() {
-        ctx.fillRect(this.x, this.y, this.width, this.height);
+        ctx.strokeRect(this.x, this.y, this.width, this.height);
+        ctx.drawImage(this.image, this.frame * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
     }
 }
+
+function drawScore() {
+    ctx.fillStyle = 'black';
+    ctx.fillText(`Score: ${score}`, 50, 75);
+    ctx.fillStyle = 'white';
+    ctx.fillText(`Score: ${score}`, 55, 80);
+}
+
+window.addEventListener('click', function(e) {
+    const detectPixelColor = ctx.getImageData(e.x, e.y, 1, 1);
+    console.log(detectPixelColor);
+});
 
 
 function animate(timestamp) {
@@ -39,7 +73,8 @@ function animate(timestamp) {
         ravens.push(new Raven());
         timeToNextRaven = 0;
     };
-    [...ravens].forEach(object => object.update());
+    drawScore();
+    [...ravens].forEach(object => object.update(deltatime));
     [...ravens].forEach(object => object.draw());
     ravens = ravens.filter(object => !object.markedForDeletion);
     requestAnimationFrame(animate);
